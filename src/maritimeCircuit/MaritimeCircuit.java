44c19b08364a00177d01157167de37a2c0e83d01
Yoel Ventoso <yoel.ventoso@gmail.com>
2023-11-18 19:48:06 -0300

    upload package | maritimeCircuit

---
+++ src/maritimeCircuit/MaritimeCircuit.java
@@ -0,0 +1,5 @@
+package maritimeCircuit;
+
+public class MaritimeCircuit {
+
+}
---
+++ test/maritimeCircuit/MaritimeCircuitTest.java
@@ -0,0 +1,14 @@
+package maritimeCircuit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MaritimeCircuitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
