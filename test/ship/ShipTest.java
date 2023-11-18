43b8df58e47cfd6d39f7ec2cdae3e23dff23d8f5
Yoel Ventoso <yoel.ventoso@gmail.com>
2023-11-18 19:52:56 -0300

    upload package | ship

---
+++ test/ship/ShipTest.java
@@ -0,0 +1,14 @@
+package ship;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShipTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
