c57e5775393e66606c2595af07defd6364993ccc
Yoel Ventoso <yoel.ventoso@gmail.com>
2023-11-18 15:44:46 -0300

    upload | Terminal, ManagedTerminal, ManagedTerminalTest

---
+++ test/terminal/ManagedTerminalTest.java
@@ -0,0 +1,14 @@
+package terminal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ManagedTerminalTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
