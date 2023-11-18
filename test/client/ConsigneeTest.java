cf791355a55a40a29a4787f3441b247f2fc76e06
Yoel Ventoso <yoel.ventoso@gmail.com>
2023-11-18 15:50:44 -0300

    upload | Client, Consignee, Shipper, ConsigneeTest, ShipperTest

---
+++ test/client/ConsigneeTest.java
@@ -0,0 +1,14 @@
+package client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConsigneeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
