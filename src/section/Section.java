8a2f11777e5fc75a00b8f86205a24f3b8e1a9e67
Yoel Ventoso <yoel.ventoso@gmail.com>
2023-11-18 19:49:44 -0300

    upload package | section

---
+++ src/section/Section.java
@@ -0,0 +1,5 @@
+package section;
+
+public class Section {
+
+}
---
+++ test/section/SectionTest.java
@@ -0,0 +1,14 @@
+package section;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SectionTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
