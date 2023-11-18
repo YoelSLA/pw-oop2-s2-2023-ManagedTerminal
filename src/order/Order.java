891bc53cc11b07c04ff17774ae042ded4ae3915e
Yoel Ventoso <yoel.ventoso@gmail.com>
2023-11-18 15:55:34 -0300

    upload | Order, ExportOrder, ImportOrder, ImportOrderTest,
    ExportOrderTest

---
+++ src/order/Order.java
@@ -0,0 +1,5 @@
+package order;
+
+public abstract class Order {
+
+}
---
+++ test/order/ExportOrderTest.java
@@ -0,0 +1,14 @@
+package order;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ExportOrderTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
