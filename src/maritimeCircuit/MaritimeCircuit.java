e61e535a285456fa944fc3d586201b5c24123855
Yoel Ventoso <yoel.ventoso@gmail.com>
2023-11-18 21:15:37 -0300

    upload test and code of MaritimeCircuit

--- src/maritimeCircuit/MaritimeCircuit.java
+++ src/maritimeCircuit/MaritimeCircuit.java
@@ -1,5 +1,33 @@
 package maritimeCircuit;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import section.Section;
+import terminal.Terminal;
+
 public class MaritimeCircuit {
+
+	private List<Section> sections;
+
+	public MaritimeCircuit() {
+		this.sections = new ArrayList<Section>();
+	}
+
+	public void addSection(Section section) {
+		sections.add(section);
+	}
+
+	public Double getPrice() {
+		return sections.stream().mapToDouble(Section::getPrice).sum();
+	}
+
+	public List<Section> getSections() {
+		return sections;
+	}
+
+	public boolean itHasASectionWhereItIs(Terminal terminal) {
+		return sections.stream().anyMatch(section -> section.isItHasATerminal(terminal));
+	}
 
 }
