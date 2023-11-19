1c2a89c6ead62a2239a83eb81922db79c384c3d4
Yoel Ventoso <yoel.ventoso@gmail.com>
2023-11-18 22:26:14 -0300

    upload truck

---
+++ test/truck/TruckTest.java
@@ -0,0 +1,14 @@
+package truck;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TruckTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
