e36373b279e45b039ec3342d392f6fe1955691d7
Yoel Ventoso <yoel.ventoso@gmail.com>
2023-11-18 21:58:44 -0300

    upload test and code of ShippingCompany

--- src/shippingCompany/ShippingCompany.java
+++ src/shippingCompany/ShippingCompany.java
@@ -1,5 +1,64 @@
 package shippingCompany;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import maritimeCircuit.MaritimeCircuit;
+import ship.Ship;
+import terminal.Terminal;
+import trip.Trip;
+
 public class ShippingCompany {
+
+	private Integer cuit;
+	private List<MaritimeCircuit> maritimeCircuits;
+	private String name;
+	private List<Ship> ships;
+	private List<Trip> trips;
+
+	public ShippingCompany(Integer cuit, String name) {
+		this.cuit = cuit;
+		this.maritimeCircuits = new ArrayList<MaritimeCircuit>();
+		this.name = name;
+		this.ships = new ArrayList<Ship>();
+		this.trips = new ArrayList<Trip>();
+	}
+
+	public void addMaritimeCircuit(MaritimeCircuit maritimeCircuit) {
+		maritimeCircuits.add(maritimeCircuit);
+	}
+
+	public void addShip(Ship ship) {
+		ships.add(ship);
+	}
+
+	public void addTrip(Trip trip) {
+		trips.add(trip);
+	}
+
+	public Integer getCuit() {
+		return cuit;
+	}
+
+	public List<MaritimeCircuit> getMaritimeCircuits() {
+		return maritimeCircuits;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public List<Ship> getShips() {
+		return ships;
+	}
+
+	public List<Trip> getTrips() {
+		return trips;
+	}
+
+	public List<MaritimeCircuit> maritimeCircuitsWhereTheTerminal(Terminal terminal) {
+		return maritimeCircuits.stream().filter(m -> m.itHasASectionWhereItIs(terminal)).collect(Collectors.toList());
+	}
 
 }
---
+++ test/shippingCompany/ShippingCompanyTest.java
@@ -0,0 +1,81 @@
+package shippingCompany;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import maritimeCircuit.MaritimeCircuit;
+import section.Section;
+import terminal.ManagedTerminal;
+import terminal.Terminal;
+
+class ShippingCompanyTest {
+
+	private ShippingCompany company; // SUT
+	private Terminal terminalBuenosAires; // DOC
+	private Terminal terminalMontevideo; // DOC
+	private Terminal terminalRioDeJaneiro; // DOC
+	private Section sectionRioDeJaneiroMontevideo; // DOC
+	private Section sectionMontevideoRioDeJaneiro; // DOC
+	private Section sectionBuenosAiresMontevideo; // DOC
+	private Section sectionMontevideoBuenosAires; // DOC
+	private MaritimeCircuit maerskLine; // DOC
+	private MaritimeCircuit maerskLineTwo; // DOC
+
+	@BeforeEach
+	void setUp() {
+		company = new ShippingCompany(42341174, "Company");
+		terminalBuenosAires = mock(ManagedTerminal.class);
+		terminalMontevideo = mock(ManagedTerminal.class);
+		terminalRioDeJaneiro = mock(ManagedTerminal.class);
+		sectionBuenosAiresMontevideo = new Section(terminalBuenosAires, terminalMontevideo, 50.0, Duration.ofHours(2));
+		sectionMontevideoBuenosAires = new Section(terminalMontevideo, terminalBuenosAires, 20.0, Duration.ofHours(4));
+		sectionRioDeJaneiroMontevideo = new Section(terminalRioDeJaneiro, terminalMontevideo, 70.0,
+				Duration.ofHours(1));
+		sectionMontevideoRioDeJaneiro = new Section(terminalMontevideo, terminalRioDeJaneiro, 100.0,
+				Duration.ofHours(10));
+		maerskLine = new MaritimeCircuit();
+		maerskLineTwo = new MaritimeCircuit();
+
+		maerskLine.addSection(sectionBuenosAiresMontevideo);
+		maerskLine.addSection(sectionMontevideoBuenosAires);
+
+		maerskLineTwo.addSection(sectionRioDeJaneiroMontevideo);
+		maerskLineTwo.addSection(sectionMontevideoRioDeJaneiro);
+
+	}
+
+	@Test
+	void testAShippingCompanyHasTwoMaritimeCircuits() {
+
+		company.addMaritimeCircuit(maerskLine);
+		company.addMaritimeCircuit(maerskLineTwo);
+
+		assertEquals(2, company.getMaritimeCircuits().size());
+
+	}
+
+	@Test
+	void testAShippingCompanyHasMaritimeCircuitsWhereTheTerminalIsLocated() {
+
+		company.addMaritimeCircuit(maerskLine);
+		company.addMaritimeCircuit(maerskLineTwo);
+
+		assertEquals(1, company.maritimeCircuitsWhereTheTerminal(terminalBuenosAires).size());
+
+	}
+
+	@Test
+	void testAShippingCompanyHasNotAMaritimeCircuitsWhereTheTerminalIsLocated() {
+
+		company.addMaritimeCircuit(maerskLineTwo);
+
+		assertEquals(0, company.maritimeCircuitsWhereTheTerminal(terminalBuenosAires).size());
+
+	}
+
+}
