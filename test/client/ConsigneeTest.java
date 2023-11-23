4f2dba101667edce2c7dfe614926075c1c1fd8bb
alejandrabesel <alejandrabesel24@gmail.com>
2023-11-23 16:50:51 -0300

    Agrego Consignee y Shipper tests

--- test/client/ConsigneeTest.java
+++ test/client/ConsigneeTest.java
@@ -2,13 +2,27 @@ package client;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
 class ConsigneeTest {
-
+	
+	private Consignee consignee;
+	
+	@BeforeEach
+	void setUp() {
+		consignee = new Consignee(12567890, "Raul Gonzalez");
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void getDni() {
+		assertEquals(12567890, consignee.getDni());
+	}
+	
+	@Test
+	void getName() {
+		assertEquals("Raul Gonzalez", consignee.getName());
 	}
 
 }
