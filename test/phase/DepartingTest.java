28db9143875a2af8975d3af575d7c164615cf32d
alejandrabesel <alejandrabesel24@gmail.com>
2023-11-23 17:17:09 -0300

    Add arrivedTest y departingTest

--- test/phase/DepartingTest.java
+++ test/phase/DepartingTest.java
@@ -2,13 +2,29 @@ package phase;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class DepartingTest {
-
+	
+	private Departing currentPhase; //SUT
+	private Outbound nextPhase; //DOC
+	
+	
+	@BeforeEach
+	void setUp() {
+		currentPhase = new Departing();
+		nextPhase = new Outbound();
+	}
+	/* no funciona ya que me devuelve dos instancias distintas de la clase Working.
+	 * Una solucion seria pasarle en el constructor a cada fase la proxima clase, de forma que podamos setearle la instancia desde el test,
+	 * pero asi rompemos con el ppio de encapsulamiento y open-closed porque estamos dejando que cualquiera pueda setearle una clase*/
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void getNextPhase() {
+		assertEquals(nextPhase, currentPhase.nextPhase());  
 	}
 
 }
