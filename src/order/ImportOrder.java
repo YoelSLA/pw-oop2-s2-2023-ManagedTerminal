bbc84c75a545b2ea74ace4d87112dee47b2fd63a
alejandrabesel <alejandrabesel24@gmail.com>
2023-11-23 11:42:50 -0300

    Agrego import order y order tests

--- src/order/ImportOrder.java
+++ src/order/ImportOrder.java
@@ -1,5 +1,23 @@
 package order;
 
-public class ImportOrder extends Order {
+import java.util.List;
 
+import client.Consignee;
+import service.Service;
+import load.Load;
+import trip.Trip;
+
+
+public class ImportOrder extends Order {
+	private Consignee consignee;
+
+	public ImportOrder(List<Service> servicesList, Load load, Consignee consignee, Trip trip) {
+		super(servicesList, load, trip);
+		this.consignee = consignee;
+	}
+	
+	public Consignee getConsignee() {
+		return consignee;
+	}
+	
 }
