b999e1c98d9ab3ca8619a6055b68884dab235123
USUARIO <USUARIO@10.28.0.153>
2023-11-24 17:19:05 -0300

    remove methods | Terminal

--- src/terminal/Terminal.java
+++ src/terminal/Terminal.java
@@ -1,7 +1,5 @@
 package terminal;
 
-import java.util.Objects;
-
 public abstract class Terminal {
 
 	private String name;
@@ -13,24 +11,5 @@ public abstract class Terminal {
 	public String getName() {
 		return name;
 	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(name);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Terminal other = (Terminal) obj;
-		return Objects.equals(name, other.name);
-	}
 	
-	
-
 }
