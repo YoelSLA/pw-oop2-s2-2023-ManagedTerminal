59048f7aa7508d2f3f364bc410a80ee4a0b0db2e
alejandrabesel <alejandrabesel24@gmail.com>
2023-12-07 01:50:34 -0300

    Add Consignee and Shipper tests for emailSender

--- src/client/Consignee.java
+++ src/client/Consignee.java
@@ -12,19 +12,20 @@ public class Consignee extends Client {
 	@Override
 	public void sendMailAboutBill(ManagedTerminal managedTerminal, Client client, Bill bill) {
 		// TODO Auto-generated method stub
-		
+		bill.printInvoice();
 	}
 
 	@Override
 	public void sendMailAboutShipInminentArrival(ManagedTerminal managedTerminal, Client client, String string) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Your ship is closed to " + managedTerminal.getName());
+//		System.out.println("Please have in mind you have until 6 hours from the ship arrival until we start charging excess storage");
 	}
 
 	@Override
 	public void sendMailAboutShipArrival(ManagedTerminal managedTerminal, Client client, String String) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Your ship has arrived to " + managedTerminal.getName());
 	}
 
 }
